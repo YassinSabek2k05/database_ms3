15290b268a349804b6c59cf6b0a76c2f248f063e
MazenAbdou <mazen.abdou@student.guc.edu.eg>
2025-12-05 16:59:36 +0200

    Add academic routes for various submissions and approvals

--- routes/academicRoutes.js
+++ routes/academicRoutes.js
@@ -1,3 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const academicController = require('../controllers/academicController');
+
+router.get('/academicdashboard', academicController.getacademicDashboard);
+
+
+
+router.post('/submitAccidental', academicController.submitAccidental);
+router.post('/submitMedical', academicController.submitMedical);
+router.post('/submitUnpaid', academicController.submitUnpaid);
+router.post('/submitCompensation', academicController.submitCompensation);
+
+router.post('/approveUnpaid', academicController.approveUnpaid);
+router.post('/approveAnnual', academicController.approveAnnual);
+
+router.post('/evaluateEmployee', academicController.evaluateEmployee);
+
+
+module.exports = router;
